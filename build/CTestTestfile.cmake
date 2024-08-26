d73bdea989eb433f587f420a74ee668f8d6fcbe7
grahanoi <noiringraham65@gmail.com>
2024-08-26 21:16:41 +0200

    base of roboter scelette

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/noi/pw2/src/RoboGardener/description
+# Build directory: /home/noi/pw2/src/RoboGardener/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
