c767fddeb57d9db92735e586b03bef9bb640b6bd
grahanoi <noiringraham65@gmail.com>
2024-08-26 17:28:40 +0200

    created a rviz config file, changes name of urdf file back to og

--- description/launch/view_robot.launch.py
+++ description/launch/view_robot.launch.py
@@ -9,13 +9,14 @@ def generate_launch_description():
             executable='robot_state_publisher',
             output='screen',
             parameters=[{
-                'robot_description': Command(['xacro', '/home/noi/pw2_ws/src/RoboGardener/description/urdf/robo_body.urdf'])
+                'robot_description': Command(['xacro ', '/home/noi/pw2/src/RoboGardener/description/urdf/base_link.urdf'])
             }]
         ),
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            output='screen'
+            output='screen',
+            arguments=['-d', '/home/noi/pw2/src/RoboGardener/description/robogardner_config.rviz']
         )
     ])
