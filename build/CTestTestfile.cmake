e5061390cc3d9399b7c07623cf9644835f758382
grahanoi <noiringraham65@gmail.com>
2024-08-26 22:38:52 +0200

    clean repo

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/noi/pw2/src/RoboGardener/description
-# Build directory: /home/noi/pw2/src/RoboGardener/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
