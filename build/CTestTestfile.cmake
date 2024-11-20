394578a488e099425c36d378654fc8aa599b4d41
grahanoi <164927679+grahanoi@users.noreply.github.com>
2024-11-20 15:50:13 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/noi/robogardner24/scr/RoboGardener/description
-# Build directory: /home/noi/robogardner24/scr/RoboGardener/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
