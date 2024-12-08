bc6d349e13df24de7d5302d2571925032f9ee502
grahanoi <noiringraham65@gmail.com>
2024-12-08 12:24:09 +0100

    add gz, ros2 control, tf2_ros and controller manager node to launch file

--- model/launch/model_motor.launch.py
+++ model/launch/model_motor.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     # Path to the URDF/Xacro file
@@ -25,11 +26,79 @@ def generate_launch_description():
             output='screen',
             arguments=['-d', '/home/noi/robogardener/scr/RoboGardener/model/config/batmobil_config.rviz']  # Path to your RViz config file
         ),
+        
         # Node for MotorGui
+        #Node(
+        #    package='serial_motor_demo',
+        #    executable='gui',
+        #    name='motor_gui',
+        #    output='screen'
+        #),
+        # Joint State Publisher
         Node(
-            package='serial_motor_demo',
-            executable='gui',
-            name='motor_gui',
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
             output='screen'
         ),
+
+        # Gazebo starten mit "gz sim"
+        ExecuteProcess(
+            cmd=['gz', 'sim', '-r', 'empty.sdf'],
+            output='screen'
+        ),
+        
+        Node(
+        package='ros_gz_sim',
+        executable='create',
+        name='spawn_robot',
+        arguments=[
+            '-topic', '/robot_description',  # Beschreibung des Roboters
+            '-name', 'batmobile'  # Name des Roboters in Gazebo
+            ],
+            output='screen',
+        ),
+        # Gazebo-ROS bridge
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='tf_bridge',
+            arguments=[
+                '/world/default/tf@tf2_msgs/msg/TFMessage[tf2_msgs/msg/TFMessage',
+                '/world/default/odom@nav_msgs/msg/Odometry[nav_msgs/msg/Odometry'
+            ],
+            output='screen'
+        ),
+
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['diff_cont'],
+            output='screen',
+        ),
+
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['joint_broad'],
+            output='screen',
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_tf_pub',
+            arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_link'],
+            output='screen'
+        ),
+        Node(
+            package='controller_manager',
+            executable='ros2_control_node',
+            name='ros2_control_node',
+            output='screen',
+            parameters=[
+                {'robot_description': Command(['xacro ', urdf_file_path])}
+            ]
+        ),
+
+
     ])
